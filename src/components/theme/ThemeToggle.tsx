63c35a706a97a478a286bc2b5bfb95d68d45f78b
gabrielfigueiredol <gabrieellfigueiredo@gmail.com>
2024-03-23 02:02:31 -0300

    feat: implement sr-only to themeToggle button

--- src/components/theme/ThemeToggle.tsx
+++ src/components/theme/ThemeToggle.tsx
@@ -16,6 +16,9 @@ export function ThemeToggle() {
 
   return (
     <Button variant="ghost" onClick={themeToggle}>
+      <span className="sr-only">
+        Trocar para {theme === 'dark' ? 'modo claro' : 'modo escuro'}
+      </span>
       {theme === 'dark' ? <Sun /> : <Moon />}
     </Button>
   )
