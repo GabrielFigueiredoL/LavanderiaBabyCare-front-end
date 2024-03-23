7b9920497c075326390dd0ada1bd39f7cbe7c666
gabrielfigueiredol <gabrieellfigueiredo@gmail.com>
2024-03-23 02:04:16 -0300

    feat: implement mobile responsiveness to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,43 @@
-import { Bolt, HandCoins, Home, LogIn, LogOut } from 'lucide-react'
+import { Bolt, HandCoins, Home, LogOut, Menu, X } from 'lucide-react'
+import { useState } from 'react'
 
 import logo from '../assets/logo.svg'
 import { NavLink } from './Nav-Link'
 import { ThemeToggle } from './theme/ThemeToggle'
+import { Button } from './ui/button'
 import { Separator } from './ui/separator'
 
 export function Header() {
+  const [open, setOpen] = useState(false)
   return (
-    <div className="border-b">
-      <div className="flex h-16 items-center gap-6 px-6">
-        <img src={logo} alt="" width={110} />
-        <Separator orientation="vertical" className="h-6" />
-        <nav className="flex items-center space-x-4 lg:space-x-6">
+    <div>
+      <div className="items-center justify-between border-b px-7 py-4 md:flex md:px-10">
+        <div className="flex items-center gap-4">
+          <img src={logo} alt="" width={110} />
+          <Separator orientation="vertical" className="h-6" />
+          <ThemeToggle />
+        </div>
+
+        <Button
+          variant="ghost"
+          type="button"
+          size="icon"
+          onClick={() => setOpen(!open)}
+          className="absolute right-8 top-4 md:hidden"
+        >
+          {open ? <X /> : <Menu />}
+        </Button>
+
+        <nav
+          className={`absolute left-0 z-20 w-screen bg-background py-2 pl-9 text-lg md:static md:z-auto md:flex md:w-auto md:items-center md:gap-4 md:py-0 md:pl-0 ${open ? 'top-20' : '-top-60'}`}
+        >
           <NavLink to="/">
             <Home className="h-4 w-4" />
             Início
           </NavLink>
-          <NavLink to="/delivery">
+          <NavLink to="/deliveries">
             <LogOut className="h-4 w-4" />
             Entregas
-          </NavLink>
-          <NavLink to="/withdraw">
-            <LogIn className="h-4 w-4" />
-            Retiradas
           </NavLink>
           <NavLink to="/finances">
             <HandCoins className="h-4 w-4" />
@@ -33,9 +48,6 @@ export function Header() {
             Configurações
           </NavLink>
         </nav>
-        <div className="ml-auto flex items-center gap-2">
-          <ThemeToggle />
-        </div>
       </div>
     </div>
   )
