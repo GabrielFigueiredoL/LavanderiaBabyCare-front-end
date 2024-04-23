e2961b0678c71e48988b08221ae05d66a9ab1bc1
gabrielfigueiredol <gabrieellfigueiredo@gmail.com>
2024-04-23 02:37:13 -0300

    feat: add Toaster and query-client provider to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import './global.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router-dom'
 
-import { ThemeProvider } from './components/theme/ThemeProvider'
+import { ThemeProvider } from '@/components/theme/ThemeProvider'
+import { Toaster } from '@/components/ui/sonner'
+import { queryClient } from '@/lib/react-query'
+
 import { router } from './routes'
 
 export function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="babycare-theme">
-      <RouterProvider router={router} />
+      <Toaster richColors />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ThemeProvider>
   )
 }
--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom'
 
 import { Header } from '@/components/Header'
-import { Toaster } from '@/components/ui/sonner'
 
 export function AppLayout() {
   return (
@@ -10,7 +9,6 @@ export function AppLayout() {
 
       <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
         <Outlet />
-        <Toaster richColors />
       </div>
     </div>
   )
